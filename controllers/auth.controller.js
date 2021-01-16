118b0f6c70799f148f6091698c44eb9e3dc0f0ba
omar abu saada <omarabusaada93@gmail.com>
2021-01-16 22:53:00 +0300

    added basic auth controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,18 +1,50 @@
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
+const { pick, some, isNil } = require("lodash");
+const { Cart } = require("../models/cart.model");
+const bcrypt = require("bcrypt");
 const { User } = require("../models/user.model");
+const { environment } = require("../environment");
 
 function constructModule() {
   return {
     /** @type {import("express").RequestHandler} */
-    authenticate(req, res, next){
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({status: ReasonPhrases.INTERNAL_SERVER_ERROR});
+    authenticate(req, res, next) {
+      res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .send({ status: ReasonPhrases.INTERNAL_SERVER_ERROR });
+      const username = req.body["username"];
+      const password = req.body["password"];
+
       throw new Error("authentication not implemented");
     },
+
     /** @type {import("express").RequestHandler} */
     addUser(req, res, next) {
-      const user = new User(req.body);
-      user
+      const userData = pick(req.body, [
+        "username",
+        "password",
+        "email",
+        "firstname",
+        "lastname",
+        "dateofbirth",
+      ]);
+      if (some(userData, isNil)) {
+        throw new Error("user data incomplete");
+      }
+      userData.password = bcrypt.hashSync(
+        userData.password,
+        environment().hashRounds
+      );
+      new Cart({ items: [] })
         .save()
+        .then((cart) => {
+          const user = new User({ ...userData, cart });
+          return Promise.all(user.save(), cart);
+        })
+        .then(([user, cart]) => {
+          cart.user = user;
+          return cart.save().then((cart) => user);
+        })
         .then((user) => {
           res
             .status(StatusCodes.CREATED)
