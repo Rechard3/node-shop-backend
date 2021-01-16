b0e63b6bb0dda0dfe9ade955ceb536a57f87a03a
omar abu saada <omarabusaada93@gmail.com>
2021-01-16 22:53:13 +0300

    modified cart to use session

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -2,10 +2,11 @@ const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 const { Cart } = require("../models/cart.model");
 const { Product } = require("../models/product.model");
 const _ = require("lodash");
+const { User } = require("../models/user.model");
 
 /** @type {import("express").RequestHandler} */
 async function addProductToCart(req, res, next) {
-  const user = req.user;
+  const user = await User.findById(req.session.user._id);
   const product = await Product.findById(req.body["id"]);
   const cart = await Cart.findById(user.cart);
   cart
@@ -24,16 +25,15 @@ async function addProductToCart(req, res, next) {
       res
         .status(StatusCodes.INTERNAL_SERVER_ERROR)
         .send({ status: ReasonPhrases.INTERNAL_SERVER_ERROR });
-    });
+    }); 
 }
 
 /** @type {import("express").RequestHandler} */
 function fetchCart(req, res, next) {
-  Cart.findById(req.user.cart)
-    .select("items")
+  User.findById(req.session.user._id)
+    .then((user) => Cart.findById(user.cart).select("items"))
     .then((cart) => {
       const { items } = cart.toObject();
-
       const res = items.map((item) => ({
         ...item.product,
         quantity: item.quantity,
@@ -61,19 +61,25 @@ function removeProductFromCart(req, res, next) {
   Cart.findById(req.user.cart)
     .then((cart) => {
       let newItems = [...cart.toObject().items];
-      const prodIdx = newItems.findIndex(({product})=>product._id.toString() == prodId.toString());
-      if(prodIdx>=0){
+      const prodIdx = newItems.findIndex(
+        ({ product }) => product._id.toString() == prodId.toString()
+      );
+      if (prodIdx >= 0) {
         const tgtItem = newItems[prodIdx];
         tgtItem.quantity = Math.max(0, tgtItem.quantity - amount);
-        if(tgtItem.quantity <= 0){
-          newItems = newItems.filter(p=>p.product._id.toString()!=prodId.toString());
+        if (tgtItem.quantity <= 0) {
+          newItems = newItems.filter(
+            (p) => p.product._id.toString() != prodId.toString()
+          );
         }
       }
       cart.items = newItems;
       return cart.save();
     })
-    .then(cart=>{
-      res.status(StatusCodes.OK).send({status: ReasonPhrases.OK, model: cart});
+    .then((cart) => {
+      res
+        .status(StatusCodes.OK)
+        .send({ status: ReasonPhrases.OK, model: cart });
     })
     .catch((err) => {
       console.log(err);
